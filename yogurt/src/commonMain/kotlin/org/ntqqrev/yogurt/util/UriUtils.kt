972e12a8b2021f6a1d659107bf65478480254d17
Wesley F. Young <wesley.f.young@outlook.com>
2025-10-31 14:23:30 +0800

    fix(yogurt): use IO dispatcher

--- yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/util/UriUtils.kt
+++ yogurt/src/commonMain/kotlin/org/ntqqrev/yogurt/util/UriUtils.kt
@@ -4,6 +4,7 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
 import kotlinx.io.buffered
 import kotlinx.io.files.Path
@@ -14,24 +15,22 @@ import kotlin.io.encoding.Base64
 
 val httpClient = createHttpClient()
 
-suspend fun resolveUri(uri: String): ByteArray = when {
-    uri.startsWith("file://") -> {
-        val filePath = uri.removePrefix("file://")
-        withContext(Dispatchers.Default) {
-            SystemFileSystem.source(Path(filePath.decodeURLPart()))
-                .buffered()
-                .use { it.readByteArray() }
+suspend fun resolveUri(uri: String): ByteArray = withContext(Dispatchers.IO) {
+    when {
+        uri.startsWith("file://") -> {
+            val filePath = uri.removePrefix("file://")
+            SystemFileSystem.source(Path(filePath.decodeURLPart())).buffered().use { it.readByteArray() }
         }
-    }
 
-    uri.startsWith("http://") || uri.startsWith("https://") -> {
-        httpClient.get(uri).readRawBytes()
-    }
+        uri.startsWith("http://") || uri.startsWith("https://") -> {
+            httpClient.get(uri).readRawBytes()
+        }
 
-    uri.startsWith("base64://") -> {
-        val base64Data = uri.removePrefix("base64://")
-        Base64.decode(base64Data)
-    }
+        uri.startsWith("base64://") -> {
+            val base64Data = uri.removePrefix("base64://")
+            Base64.decode(base64Data)
+        }
 
-    else -> throw IllegalArgumentException("Unsupported URI scheme: $uri")
+        else -> throw IllegalArgumentException("Unsupported URI scheme: $uri")
+    }
 }
