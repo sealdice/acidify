4c8d77cf8d0a94b9d191c55b28a5f0c9ffe9a915
Wei FeiYang <wesley.f.young@outlook.com>
2025-11-03 11:49:12 +0800

    feat: add acidify-core publish workflow

--- acidify-core/build.gradle.kts
+++ acidify-core/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("buildsrc.convention.kotlin-multiplatform")
     alias(libs.plugins.kotlin.serialization)
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
+
+group = "org.ntqqrev"
+version = "0.1.0"
 
 kotlin {
     sourceSets {
@@ -30,6 +34,41 @@ kotlin {
         }
         linuxMain.dependencies {
             implementation(libs.ktor.client.curl)
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(
+        groupId = project.group.toString(),
+        artifactId = project.name,
+        version = project.version.toString()
+    )
+
+    pom {
+        name = project.name
+        description = "Kotlin NTQQ protocol implementation"
+        url = "https://github.com/LagrangeDev/acidify"
+        inceptionYear = "2025"
+        licenses {
+            license {
+                name = "GNU General Public License v3.0"
+                url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+            }
+        }
+        developers {
+            developer {
+                id = "Wesley-Young"
+                name = "Wesley F. Young"
+                email = "wesley.f.young@outlook.com"
+            }
+        }
+        scm {
+            connection = "scm:git:git://github.com/LagrangeDev/acidify.git"
+            developerConnection = "scm:git:ssh://github.com/LagrangeDev/acidify.git"
+            url = "https://github.com/LagrangeDev/acidify"
         }
     }
 }
