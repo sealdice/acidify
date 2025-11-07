dfc9c1a7c226fee024f6edcafbe447a59c21c516
Wei FeiYang <wesley.f.young@outlook.com>
2025-11-07 20:46:44 +0800

    docs: remove Inter font

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,15 +1,6 @@
 import { LastUpdated, Layout, Navbar } from 'nextra-theme-docs';
 import { getPageMap } from 'nextra/page-map';
 import 'nextra-theme-docs/style.css';
-import '@fontsource/inter/100.css';
-import '@fontsource/inter/200.css';
-import '@fontsource/inter/300.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-import '@fontsource/inter/800.css';
-import '@fontsource/inter/900.css';
 import './styles.css';
 import { Head, Search } from 'nextra/components';
 import { Metadata } from 'next';
