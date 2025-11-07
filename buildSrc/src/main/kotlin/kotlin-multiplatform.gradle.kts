10ac4c70dc6f099b7d741099df57bfc43e92c263
Wesley F. Young <wesley.f.young@outlook.com>
2025-11-07 16:19:46 +0800

    update: add check for jvm targeted build

--- buildSrc/src/main/kotlin/kotlin-multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-multiplatform.gradle.kts
@@ -7,20 +7,19 @@ plugins {
 kotlin {
     jvm()
 
-    // val isBuildingCoreLibrary = System.getenv("GITHUB_ACTIONS_BUILD_CORE_LIBRARY") == "true"
     val actionsTarget = System.getenv("GITHUB_ACTIONS_BUILD_TARGET")
-    // val hostOs = System.getProperty("os.name")
-    // val arch = System.getProperty("os.arch")
-    when {
+    when (actionsTarget) {
         // is run from GitHub Actions - build Yogurt, improving dependency pulling time
-        actionsTarget == "mingwX64" -> mingwX64()
-        actionsTarget == "macosX64" -> macosX64()
-        actionsTarget == "macosArm64" -> macosArm64()
-        actionsTarget == "linuxX64" -> linuxX64()
-        actionsTarget == "linuxArm64" -> linuxArm64()
+        "jvm" -> {} // already added
+        "mingwX64" -> mingwX64()
+        "macosX64" -> macosX64()
+        "macosArm64" -> macosArm64()
+        "linuxX64" -> linuxX64()
+        "linuxArm64" -> linuxArm64()
 
         // is run locally
         else -> {
+            jvm()
             mingwX64()
             macosX64()
             macosArm64()
