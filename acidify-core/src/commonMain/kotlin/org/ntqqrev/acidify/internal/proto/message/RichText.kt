274e46221a0142c117390a410ca64c11dc49cbee
Wei FeiYang <wesley.f.young@outlook.com>
2026-02-08 01:16:16 +0800

    fix(core): repair RichText defaults, thus repair forward

--- acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/internal/proto/message/RichText.kt
+++ acidify-core/src/commonMain/kotlin/org/ntqqrev/acidify/internal/proto/message/RichText.kt
@@ -5,10 +5,10 @@ import kotlinx.serialization.protobuf.ProtoNumber
 
 @Serializable
 internal class RichText(
-    @ProtoNumber(1) val attr: Attr = Attr(),
+    @ProtoNumber(1) val attr: Attr? = null,
     @ProtoNumber(2) val elems: List<Elem> = emptyList(),
-    @ProtoNumber(3) val notOnlineFile: NotOnlineFile = NotOnlineFile(),
-    @ProtoNumber(4) val ptt: Ptt = Ptt(),
-    @ProtoNumber(5) val tmpPtt: TmpPtt = TmpPtt(),
-    @ProtoNumber(6) val trans211TmpMsg: Trans211TmpMsg = Trans211TmpMsg(),
+    @ProtoNumber(3) val notOnlineFile: NotOnlineFile? = null,
+    @ProtoNumber(4) val ptt: Ptt? = null,
+    @ProtoNumber(5) val tmpPtt: TmpPtt? = null,
+    @ProtoNumber(6) val trans211TmpMsg: Trans211TmpMsg? = null,
 )
