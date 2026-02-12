c822883e12d9b6dade2f8823614fc369b53016f9
Wei FeiYang <wesley.f.young@outlook.com>
2026-02-12 21:53:01 +0800

    fix(core/js): repair build script

--- acidify-core-js/tsdown.config.ts
+++ acidify-core-js/tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ['../../acidify-core/build/dist/js/productionLibrary/acidify-acidify-core.mjs'],
+  entry: ['../acidify-core/build/dist/js/productionLibrary/acidify-acidify-core.mjs'],
   format: 'esm',
   clean: true,
   dts: false,
