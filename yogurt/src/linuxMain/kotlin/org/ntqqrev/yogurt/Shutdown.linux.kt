77c09fb67f4fac8e8c5916414ff7e6e194f6aa19
Wei FeiYang <wesley.f.young@outlook.com>
2026-03-05 17:17:28 +0800

    fix(yogurt): remove redundant Bot atomic ref

--- yogurt/src/linuxMain/kotlin/org/ntqqrev/yogurt/Shutdown.linux.kt
+++ yogurt/src/linuxMain/kotlin/org/ntqqrev/yogurt/Shutdown.linux.kt
@@ -1,22 +1,15 @@
 package org.ntqqrev.yogurt
 
 import io.ktor.server.engine.*
-import io.ktor.server.plugins.di.*
-import kotlinx.atomicfu.atomic
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.staticCFunction
-import kotlinx.coroutines.runBlocking
-import org.ntqqrev.acidify.AbstractBot
 import org.ntqqrev.yogurt.YogurtApp.t
 import platform.posix.SIGINT
 import platform.posix._exit
 import platform.posix.signal
 
-val botRef = atomic<AbstractBot?>(null)
-
 @OptIn(ExperimentalForeignApi::class)
 actual fun EmbeddedServer<*, *>.onSigint(hook: () -> Unit) {
-    botRef.value = runBlocking { application.dependencies.resolve<AbstractBot>() }
     // On Linux targets, the shutdown hook gets unexpectedly overridden by Ktor's internal shutdown hook;
     // and `server.stop()` blocks forever on Linux,
     // so we have to use a `exit(0)` to force the process to exit immediately after the shutdown hook is executed.
